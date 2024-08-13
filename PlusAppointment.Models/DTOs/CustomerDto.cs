8ae4542622a44c36995e283c1b624bacc64eeea6
JaydenLeHiep <98736015+JaydenLeHiep@users.noreply.github.com>
2024-08-13 21:14:32 +0200

    add: add class for this + forgetin the last pull request

--- PlusAppointment.Models/DTOs/CustomerDto.cs
+++ PlusAppointment.Models/DTOs/CustomerDto.cs
@@ -4,6 +4,6 @@ public class CustomerDto
 {
     
     public string? Name { get; set; }
-    public string? Email { get; set; }
-    public string? Phone { get; set; }
+    public string Email { get; set; } = String.Empty;
+    public string Phone { get; set; } = String.Empty;
 }

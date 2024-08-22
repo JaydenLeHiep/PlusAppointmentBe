04d13ab4fdbe60cb86961c2859837a0bd653311c
JaydenLeHiep <98736015+JaydenLeHiep@users.noreply.github.com>
2024-08-22 13:53:05 +0200

    integrate: integrate the signalR to send the event to browser

---
+++ PlusAppointment.Api/Utils/Hub/AppointmentHub.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.SignalR;
+namespace PlusAppointment.Utils.Hub;
+
+public class AppointmentHub: Microsoft.AspNetCore.SignalR.Hub
+{
+    // This method can be called by clients
+    public async Task SendAppointmentUpdate(string message)
+    {
+        // Send a message to all connected clients
+        await Clients.All.SendAsync("ReceiveAppointmentUpdate", message);
+    }
+}
